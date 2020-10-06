17495cff2a28742d2a59ae59937f0d4415c70ef0
TigazOne <r.tiger@hotmail.se>
2020-10-06 22:02:37 +0200

    Test

---
+++ Rickard.Tiger/7.3 inlämning/area.h
@@ -0,0 +1,13 @@
+/*
+______________________________________________________________________________________
+Date        : 2020-10-05
+File        : uppgift7.3.c
+Description : Göra funktioner och lägga in för bibliotek
+Author      : Rickard Tiger r.tiger@hotmail.se
+______________________________________________________________________________________
+*/
+#include <stdio.h>
+#define PI 3.141592653589793
+double getAreaRektangle(double, double);
+double getAreaCirkle(double);
+double getAreaTriangle(double, double);
---
+++ Rickard.Tiger/7.3 inlämning/uppgift7.3.c
@@ -0,0 +1,41 @@
+/*
+______________________________________________________________________________________
+Date        : 2020-10-05
+File        : uppgift7.3.c
+Description : Göra funktioner och lägga in för bibliotek
+Author      : Rickard Tiger r.tiger@hotmail.se
+______________________________________________________________________________________
+*/
+#include <stdio.h>
+#include "area.h"
+int main()
+{
+  //Area för rektangel
+  double base, height, areaForRektangle;
+  printf("Vad ar basen pa rektangeln? ");
+  scanf("%lf", &base);
+  printf("Vad ar hojden for rektangeln? ");
+  scanf("%lf", &height);
+  //Anrop av funktion
+  areaForRektangle = getAreaRektangle (base, height);
+  printf("Arean for rektangeln ar: %.0f", areaForRektangle);
+  printf("\n");
+  //Area för Cirkel
+  double radie, areaForCirkle;
+  printf("Vad ar radien pa cirkeln? ");
+  scanf("%lf", &radie);
+  //Anrop av funktion
+  areaForCirkle = getAreaCirkle (radie);
+  printf("Arean for cirkeln ar: %.0f", areaForCirkle);
+  printf("\n");
+  //Area för Triangel
+  double bas, hojd, areaForTriangle;
+  printf("Vad ar basen pa Triangle? ");
+  scanf("%lf", &bas);
+  printf("Vad ar hojden for Triangle? ");
+  scanf("%lf", &hojd);
+  //Anrop av funktion
+  areaForTriangle = getAreaTriangle (bas, hojd);
+  printf("Arean for rektangeln ar: %.0f", areaForTriangle);
+  return 0;
+}
