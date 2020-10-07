922121d9837266a69a1c5b46c892fbf346fcc05d
leifkruger <leif.kruger@gmail.com>
2020-10-07 11:16:47 +0200

    Uppgift 7.1

---
+++ leifkruger/uppgift_7-1_temp.h
@@ -0,0 +1,11 @@
+//==============================================================================
+//Description   : Deklarationsfil.
+//Filename      : uppgift_7-1_temp.h
+//Date          : 2020-10-06
+//Author        : Leif Krüger, leif.kruger@gmail.com
+//==============================================================================
+
+_Bool observation(double t, int h, int m);
+double aktuell_temp();
+int obs_tim(); 
+int obs_min();
