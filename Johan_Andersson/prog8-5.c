82e5613068ae70c22e55558dcbb4a30241c60ea9
J-azon <johand1994@gmail.com>
2020-10-07 13:48:19 +0200

    Uppgifter8.5

---
+++ Johan_Andersson/prog8-5.c
@@ -0,0 +1,8 @@
+//Johan Andersson, 2020-10-07 J-Azon @github
+
+#include <stdio.h>
+#include "prog8-5func.c" //innehåller "sum" & "kvad_sum".
+#include "prog8-5.h"    // innehåller deklarering utav funktioner.
+
+int main(){
+}
