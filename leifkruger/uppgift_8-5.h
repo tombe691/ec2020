5da2199e5bdab8b76c3ad84b480a8cb9fde0f1fd
leifkruger <leif.kruger@gmail.com>
2020-10-10 14:43:53 +0200

    Uppgift 8.5

---
+++ leifkruger/uppgift_8-5.h
@@ -0,0 +1,9 @@
+//==============================================================================
+//Description   : Deklarationsfil.
+//Filename      : uppgift_8-5.h
+//Date          : 2020-10-10
+//Author        : Leif Krüger, leif.kruger@gmail.com
+//==============================================================================
+
+double sum(double a[], int n);
+double kvad_sum(double a[], int n);
