6878f84a2bd947a64ba9727fcf68da3761441fa5
Niklas Karlsson <knasen1@gmail.com>
2020-10-12 08:53:10 +0200

    First commit

---
+++ niklas_karlsson/Programmeringsuppgift/7.3/area.h
@@ -0,0 +1,13 @@
+/*
+ * @Author: Niklas Karlsson
+ * @Date: 2020-10-07
+ * Copyright (c) 2020 Niklas Karlsson. All Rights Reserved.
+ */
+ #include <stdio.h>
+ // The PI with some decimals
+ #define PI 3.141592653589793
+
+// Declarations for the functions
+double areaRectangle(double base, double height);
+double areaCircle(double radius);
+double areaTriangle(double base, double height);
