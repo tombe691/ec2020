64b51ecf6633c474aa7f9f20abdf75a49bfb7634
natlee77 <natlisjo@gmail.com>
2020-10-12 15:59:44 +0200

    Uppgift 8.5

--- Nataliya_Lisjo/sum.c
+++ Nataliya_Lisjo/sum.c
@@ -1,16 +1,20 @@
-#include<stdio.h>
-
-double sum(double f[], int nr){
-   double sum = 0;
-   for (int i=0; i<nr; i++)
-    sum += f[i];
-    return sum;
-
-}
-
-double kvad_sum(double f[], int nr) {
-    double kvad_sum = 0;
-    for (int i=0; i<nr; i++)
-     kvad_sum += f[i] * f[i];
-    return kvad_sum;
+#include <stdio.h>
+#include "sum.h"
+int main()
+{
+    int nr;
+    double f[nr];
+    printf("Skriva elementen av array\n");
+    for (int i=0;i<nr;i++)
+    {     
+        scanf("%lf",&f[i]);
+        printf(" Din array f [%lf] = \n",f[i]);
+        double skapad_f[nr] = f[i] ;       
+    }
+    return skapad_f[nr];      
 }    
+ void calcul (skapad_f,nr)
+    {
+    printf("Summan av elementen =%0.2lf\n",sum(skapad_f,nr));
+    printf("Summan av elementens kvadrater = %0.2lf\n",kvad_sum(skapad_f,nr));
+    }
