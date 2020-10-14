20f7be9bd6973973698c526782761d9319f86283
somswe <72867014+somswe@users.noreply.github.com>
2020-10-14 14:57:19 +0200

    Delete uppgift 9.2.c

--- Abdullahi Aden/uppgift 9.2.c
+++ Abdullahi Aden/uppgift 9.2.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 int main(){
+	char word[100], reverseWord[100];
+	int done = 1, i;
+	
+	/*Ta in ett ord*/
+	printf("Skriv in ditt ord: ")
+	scanf("%s", word);
+	
+	/*Kopiera word strängen till reverseWord*/
+	strcpy(reverseWord, word);
+	
+	/*Ta in längden på ordet för att identifiera slutet*/
+	int end = strlen(word);
+	
+	/**/
+	do{
+	char temp[];
 	
 	
+	i++
+	} while(done == 0);
 }
