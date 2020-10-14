e331ba124030d749bec7a07c7d57154e38430766
somswe <72867014+somswe@users.noreply.github.com>
2020-10-14 15:49:07 +0200

    Update uppgift 9.2.c

--- Abdullahi Aden/uppgift 9.2.c
+++ Abdullahi Aden/uppgift 9.2.c
@@ -1,23 +1,36 @@
-#include<stdio.h>
-int main(){
-	char word[100], reverseWord[100];
-	int done = 1, i;
-	
-	/*Ta in ett ord*/
-	printf("Skriv in ditt ord: ")
-	scanf("%s", word);
-	
-	/*Kopiera word strängen till reverseWord*/
-	strcpy(reverseWord, word);
-	
-	/*Ta in längden på ordet för att identifiera slutet*/
-	int end = strlen(word);
-	
-	/**/
-	do{
-	char temp[];
-	
-	
-	i++
-	} while(done == 0);
+#include <stdio.h>
+#include <string.h>
+ /* Författare:Abdullahi Aden.
+    Datum 202.10.12.
+    Anledning: uppgift 9.2 om palindrome.*/
+ 
+int main()
+{
+    char string[2000];  
+    int i,n,c=0;
+ 
+    printf("Enter  the string : ");
+    scanf("%d",string);
+    
+    //kollar hur lång är stringan.
+    n=strlen(string);
+ 
+    for(i=0;i<n/2;i++)  
+    {
+    	if(string[i]==string[n-+i-1])
+    	c++;
+ 
+ 	}
+ 	//Om stringen lir 0 då är palindrome.
+ 	if(c==0)
+ 	    printf("string is palindrome");
+ 	    
+ 	//Annars om stringen inte blir 0 då är inte palindrome.
+    else
+        printf("string is not palindrome");
+ 
+ 	 
+     
+    return 0;
+     
 }
