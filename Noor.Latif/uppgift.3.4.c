a4bb06063c9fb0c15ed3e0598c86dec2d7864f57
Noor Latif <noor.crystal@gmail.com>
2020-10-17 19:05:19 +0200

    Uppdatering

--- Noor.Latif/uppgift.3.4.c
+++ Noor.Latif/uppgift.3.4.c
@@ -11,24 +11,25 @@
 int main (void) {
 	int gender;
 	long int id;
-	printf("Hej %s! Detta program Ã¤r bra fÃ¶r den som inte Ã¤r sÃ¤ker pÃ¥ sitt kÃ¶n.\n", getenv("USERNAME"));
+	printf("Hej %s! Detta program är bra för den som inte är säker på sitt kön.\n", getenv("USERNAME"));
 	printf("Write 0 if female, 1 if male: ");
 	scanf("%d", &gender);
-	printf("Your ID number (10 digits only): "); // Only digits work
+	printf("Your ID number (10 digits only): "); /* Only digits work */
 	scanf("%ld", &id);
 	/* To Reach the 9th digit that identifies the gender.
 	We have to divide the ID number by 10 */
 	id = (id / 10);
 	printf("To identify gender we need the 9th digit: %ld \n", id);
-	if (gender == 0 && id%2 == 0) {			// Modulo division by 2 to identify even numbers.
+	if (gender == 0 && id%2 == 0) {			/* Modulo division by 2 to identify even numbers. */
 		printf("Correct, you're female\n");
-	} else if (gender == 1 && id%2 == 1) { 	// Modulo division by 2 will output a remainder of 1 if number is uneven.
+	} else if (gender == 1 && id%2 == 1) { 	/* Modulo division by 2 will output a remainder of 1 if number is uneven. */
 		printf("Correct, you're male\n");
-	} else if (gender == 1) { 				// output error on gender mismatch
+	} else if (gender == 1) { 				/* output error on gender mismatch */
 		printf("Wrong. You're not male\n");
-	} else if (gender == 0) { 				// output error on gender mismatch
+	} else if (gender == 0) { 				/* output error on gender mismatch */
 		printf("Wrong. You're not female\n");
 	} else {
 		printf("Error. Wrong input!\n");
 	}
+   return 0;
  }
