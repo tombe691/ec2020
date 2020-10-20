8332e1803b3b97157495f52faaca78a80a9d11d5
Noor Latif <noor.crystal@gmail.com>
2020-10-20 12:06:14 +0200

    Fixed syntax errors
    Added <stdio.h>. Changed double main to int main.
    Added missing semicolons. Declared weeklyWages and changed comparator '==' to assign '='.
    Hardcoding numbers is bad practice. Replaced with scanf() functions.
    Inside printf functions:
    Changed %d to %lf (with 0 decimals)
    Moved the separate text within quotes inside the first set of quotes.
    Removed unnecessary %s placeholders
    Increased output verbosity by adding hoursPerWeek value.

--- Noor.Latif/errorsearch.c
+++ Noor.Latif/errorsearch.c
@@ -12,21 +12,33 @@
 //					of io-flags. Inserted "wait for return code" at the end.
 //		2019-10-18: Revised by Tomas. Converted to English version and VS 2012.
 //		2019-10-21: Revised by Nisse for VS 2013.
-//      
+//      2020-10-20: Revised by Noor:
+//                      Added <stdio.h>. Changed double main to int main.
+//                      Added missing semicolons. Declared weeklyWages and changed comparator '==' to assign '='.
+//                      Hardcoding numbers is bad practice. Replaced with scanf() functions.
+//                      Inside printf functions:
+//                          Changed %d to %lf (with 0 decimals)
+//                          Moved the separate text within quotes inside the first set of quotes.
+//                          Removed unnecessary %s placeholders
+//                          Increased output verbosity by adding hoursPerWeek value.
 //-----------------------------------------------------------------------
-
-double main()  
+#include <stdio.h>
+int main()
 {
-	// Define and initialize variables
-	int hoursPerWeek = 35
-	double hourlyWages = 83
-	
+	// Define and prompt user to initialize variables
+    int hoursPerWeek;
+    double hourlyWages;
+    printf("Enter hours per week: \n");
+	scanf("%d\n", &hoursPerWeek);
+    printf("Enter hourly wages (kr): \n");
+    scanf("%lf\n", &hourlyWages);
+
 	// Calculate weekly wage
-	weeklyWages == hoursPerWeek * hourlyWages;
+	double weeklyWages = hoursPerWeek * hourlyWages;
 
 	// Show result
-	printf("%s %f %s\n", "Given an hourly wage of ", hourlyWages, " kr");
-	printf("%s %d %s\n", " the weekly wages will be: ", weeklyWages, " kr");
+	printf("Given an hourly wage of %.lf kr\n", hourlyWages);
+	printf("The weekly wages, based on %d hours will be: %.lf kr\n", hoursPerWeek, weeklyWages);
 
 	return 0;
 }
