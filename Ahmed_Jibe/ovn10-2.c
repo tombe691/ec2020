4c66b50f4c23f5f83928cd96029e660540c98871
Ahmed Jibe <ahmed.jibe@gmail.com>
2020-10-23 18:52:11 +0200

    format

--- Ahmed_Jibe/ovn10-2.c
+++ Ahmed_Jibe/ovn10-2.c
@@ -3,14 +3,16 @@
 #include <string.h>
 #include "mystring.h"
 
-int main() {
-  FILE *f = open_file("Filens namn?", "r"); 
-  int r = 0, k = 0;   // antal rader och rader med kommentarer 
+int main()
+{
+  FILE *f = open_file("Filens namn?", "r");
+  int r = 0, k = 0; // antal rader och rader med kommentarer
   char s[100];
-  while (fgets(s, 100, f) != NULL) {
+  while (fgets(s, 100, f) != NULL)
+  {
     r++;
-    if (strstr(s, "//") != NULL) 
+    if (strstr(s, "//") != NULL)
       k++;
   }
-  printf("%.2f %% av raderna har kommentarer", 1.0*k/r);  // undvik heltalsdivision
-} 
+  printf("%.2f %% av raderna har kommentarer", 1.0 * k / r); // undvik heltalsdivision
+}
