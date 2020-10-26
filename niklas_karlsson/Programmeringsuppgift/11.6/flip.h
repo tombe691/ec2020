e27789bc3187e9c26cdec23cfad7694ea6fdd03d
Niklas Karlsson <knasen1@gmail.com>
2020-10-26 17:45:08 +0100

    First commit

---
+++ niklas_karlsson/Programmeringsuppgift/11.6/flip.h
@@ -0,0 +1,10 @@
+// Max integer
+#define I_MAX 20
+// Max double
+#define D_MIN 1.0
+// Max double
+#define D_MAX 1000.0
+
+void minMax(double *in, int length, double *low, double *high);
+double splitString(double *str);
+double theRand(double min, double max);
