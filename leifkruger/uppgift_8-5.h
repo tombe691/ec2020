ac6e3790dc23caa531df5107705617bdbeda68c5
leifkruger <72393240+leifkruger@users.noreply.github.com>
2020-11-02 22:25:07 +0100

    Delete uppgift_8-5.h

--- leifkruger/uppgift_8-5.h
+++
@@ -1,9 +0,0 @@
-//==============================================================================
-//Description   : Deklarationsfil.
-//Filename      : uppgift_8-5.h
-//Date          : 2020-10-10
-//Author        : Leif Krüger, leif.kruger@gmail.com
-//==============================================================================
-
-double sum(double a[], int n);
-double kvad_sum(double a[], int n);
