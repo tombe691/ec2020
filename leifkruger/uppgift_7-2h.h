3214a62dc17e64daf6e0ec812a25c38438828955
leifkruger <72393240+leifkruger@users.noreply.github.com>
2020-11-02 22:31:10 +0100

    Delete uppgift_7-2h.h

--- leifkruger/uppgift_7-2h.h
+++
@@ -1,8 +0,0 @@
-//==============================================================================
-//Description   : Deklarationsfil.
-//Filename      : uppgift_7-2h.h
-//Date          : 2020-10-07
-//Author        : Leif Krüger, leif.kruger@gmail.com
-//==============================================================================
-
-int fibonacci(void);
