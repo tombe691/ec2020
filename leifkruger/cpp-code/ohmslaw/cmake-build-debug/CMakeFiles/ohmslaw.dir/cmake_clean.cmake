9b49c5facb9bb56a36f44bddd34661b22dc71090
leifkruger <leif.kruger@gmail.com>
2020-11-04 22:41:28 +0100

    ohmslaw

---
+++ leifkruger/cpp-code/ohmslaw/cmake-build-debug/CMakeFiles/ohmslaw.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ohmslaw.dir/main.cpp.o"
+  "ohmslaw"
+  "ohmslaw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ohmslaw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
