182222024fd588fb1b03c4790fd38e167d1ac5a5
Niklas Karlsson <knasen1@gmail.com>
2020-11-06 14:26:04 +0100

    Changed spaces to tabs.

--- niklas_karlsson/Programmeringsuppgift/C++/ohmsLaw/ohm.cpp
+++ niklas_karlsson/Programmeringsuppgift/C++/ohmsLaw/ohm.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main(void)
 {
-	// initialize the class
-	ohmsLaw ohm;
+    // initialize the class
+    ohmsLaw ohm;
     // Declare variables
-	int firstChoice, secondChoice;
+    int firstChoice, secondChoice;
     // Print once.
-	cout << "Welcome to ohms law converter." << endl
-		 << "For this converter to work you'll need atleast two types."
-		 " (R, I, P and/or U)"
-		 << endl;
+    cout << "Welcome to ohms law converter." << endl
+         << "For this converter to work you'll need atleast two types."
+         " (R, I, P and/or U)"
+         << endl;
     // Make a function for this later on.
-	firstChoice = ohm.menu(0);
-	secondChoice = ohm.menu(firstChoice);
+    firstChoice = ohm.menu(0);
+    secondChoice = ohm.menu(firstChoice);
     // Caller process will get this.
-	return 0;
+    return 0;
 }
