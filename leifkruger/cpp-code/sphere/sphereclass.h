b02ea95cf4a18bbb942b83180ebc2dbf981b8596
leifkruger <leif.kruger@gmail.com>
2020-11-10 18:59:41 +0100

    Geometriprogram klot (sphere)

---
+++ leifkruger/cpp-code/sphere/sphereclass.h
@@ -0,0 +1,22 @@
+//
+// Created by leif on 2020-11-10.
+//
+
+#ifndef SPHERE_SPHERECLASS_H
+#define SPHERE_SPHERECLASS_H
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class sphere {
+private:
+    double radiusSphere;
+public:
+    void setRadius(double inputRadius);
+    void getVolume();
+    void getArea();
+};
+
+#endif //SPHERE_SPHERECLASS_H
