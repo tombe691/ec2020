0d26d55aaff6ffb297ae94e19288fbae95463f98
leifkruger <leif.kruger@gmail.com>
2020-11-10 18:59:33 +0100

    Geometriprogram klot (sphere)

---
+++ leifkruger/cpp-code/sphere/sphereclass.cpp
@@ -0,0 +1,17 @@
+//
+// Created by leif on 2020-11-10.
+//
+
+#include <iostream>
+#include <cmath>
+#include "sphereclass.h"
+
+void sphere::setRadius(double inputRadius) {
+    radiusSphere = inputRadius;
+}
+void sphere::getVolume() {
+    cout << "The Volume of the sphere is: " << (4 * M_PI * radiusSphere * radiusSphere * radiusSphere) / 3  << " volume units."  << endl;
+}
+void sphere::getArea() {
+    cout << "The Area of the sphere is: " << 4 * M_PI * radiusSphere * radiusSphere  << " area units."  << endl;
+}
