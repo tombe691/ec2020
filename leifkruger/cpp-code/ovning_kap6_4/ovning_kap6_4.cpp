26a26fcaefce8b157068fe5e5fce6618b399eaa3
leifkruger <leif.kruger@gmail.com>
2020-11-12 19:25:08 +0100

    Change use of i and index 0

--- leifkruger/cpp-code/ovning_kap6_4/ovning_kap6_4.cpp
+++ leifkruger/cpp-code/ovning_kap6_4/ovning_kap6_4.cpp
@@ -23,6 +23,7 @@ CHANGES
 REF NO	VERSION		DATE (YYMMDD)	WHO	DETAIL
 -------------------------------------------------------------------------------
 		1			2020-11-12		LK	Start date
+		2			2020-11-12		LK	Change use of i and start with index 0
 *******************************************************************************
 */
 
@@ -32,21 +33,21 @@ REF NO	VERSION		DATE (YYMMDD)	WHO	DETAIL
 using namespace std;
 
 int main() {
-    employee employer[4]; //Skip index 0
+    employee employer[3]; 
     float tempCompensation;
     cout << endl;
     cout << "Employer " << endl;
     cout << "========" << endl;
-    for (int i = 1; i < 4; i++) {
-        cout << "Employer nr: " << i << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "Employer nr: " << i+1 << endl;
         cout << "Enter compensation: ";
         cin >> tempCompensation;
         employer[i].setId(i);
         employer[i].setCompensation(tempCompensation);
     }
     cout << endl;
-    for (int i = 1; i < 4; i++) {
-        cout << "Employee nr: " << employer[i].getId() << " has compensation " << employer[i].getCompensation() << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "Employee nr: " << i+1 << " has compensation " << employer[i].getCompensation() << endl;
     }
     cout << endl;
     return 0;
