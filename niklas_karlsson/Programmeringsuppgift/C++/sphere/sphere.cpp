cd85963e39dc9783dcdffeab22b03926e5fdbad5
Niklas Karlsson <knasen1@gmail.com>
2020-11-15 20:37:32 +0100

    Added comments

--- niklas_karlsson/Programmeringsuppgift/C++/sphere/sphere.cpp
+++ niklas_karlsson/Programmeringsuppgift/C++/sphere/sphere.cpp
@@ -1,10 +1,12 @@
 #include "sphere.hpp" // 'Sphere' has not been declared if not included
 
+// Funtio to get the volume
 double Sphere::getVolume(void)
 {
     return (4 / 3) * this->PI * pow(this->radius, 3);
 }
 
+// Funtion to the the area
 double Sphere::getArea(void)
 {
     return (4 * this->radius) * this->PI * pow(this->radius, 2);
