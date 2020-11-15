17d1fa30c72c075c5b53ad72cecfbdcce9d6906e
Niklas Karlsson <knasen1@gmail.com>
2020-11-15 20:45:30 +0100

    Changed the math

--- niklas_karlsson/Programmeringsuppgift/C++/sphere/sphere.cpp
+++ niklas_karlsson/Programmeringsuppgift/C++/sphere/sphere.cpp
@@ -1,13 +1,15 @@
 #include "sphere.hpp" // 'Sphere' has not been declared if not included
 
-// Funtio to get the volume
+// Funtiond to get the volume
 double Sphere::getVolume(void)
 {
-    return (4 / 3) * this->PI * pow(this->radius, 3);
+    // (4 ⋅ π ⋅ r3) / 3:
+    return (4 * this->PI * pow(this->radius, 3)) / 3;
 }
 
 // Funtion to the the area
 double Sphere::getArea(void)
 {
-    return (4 * this->radius) * this->PI * pow(this->radius, 2);
+    // 4 ⋅ π ⋅ r2:
+    return (4 * this->PI * pow(this->radius, 2));
 }
--- niklas_karlsson/Programmeringsuppgift/C++/sphere/sphere.hpp
+++ niklas_karlsson/Programmeringsuppgift/C++/sphere/sphere.hpp
@@ -2,14 +2,17 @@
 
 class Sphere
 {
+    // Only callable from this class or friend
     private:
         double radius;
         double PI = 2 * asin(1.0);  // asin() returns the value of PI/2..
+    // Can be seen/callable everywhere.
     public:
         void setRadius(double radius)
         {
             this->radius = radius;
         }
+        // Declarations
         double getVolume(void);
         double getArea(void);
 };
