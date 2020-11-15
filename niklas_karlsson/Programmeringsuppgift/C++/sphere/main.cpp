53cc3dae99ab9909b3308a1128f24bef5a4ac5f0
Niklas Karlsson <knasen1@gmail.com>
2020-11-15 20:48:29 +0100

    Added comments

--- niklas_karlsson/Programmeringsuppgift/C++/sphere/main.cpp
+++ niklas_karlsson/Programmeringsuppgift/C++/sphere/main.cpp
@@ -12,13 +12,16 @@ int main(void)
     // variables that retrieve values ​​from the class
     double volume = newSphere.getVolume();
     double area = newSphere.getArea();
+    // Print the values and some text
     cout << "The Volume of the sphere is: " << volume << endl
          << "And the Area of the sphere is: " << area << endl << endl;
 
     // gives the radius a new value.
     newSphere2.setRadius(-3);
+    // variables that retrieve values ​​from the class
     volume = newSphere2.getVolume();
     area = newSphere2.getArea();
+    // Print the values and some text
     cout << "The Volume of the sphere is: " << volume << endl
          << "And the Area of the sphere is: " << area << endl;
 }
